4449f6cb4bc85b7d2693bd7cd61e42acb4901353
Wilma Hultén <wilmahulten@gmail.com>
2020-12-18 13:14:02 +0100

    Add files via upload

---
+++ scene.js
@@ -0,0 +1,70 @@
+let scene, camera, renderer, light, materials = [],
+backgroundColor, planeColor, numPlanes=4000, numIllustrations=16,
+geometry, planes = [], plane, planeSize = 30;
+
+function init() {
+  scene = new THREE.Scene();
+  backgroundColor = new THREE.Color(0xd6d0d0);
+  scene.background = backgroundColor;
+  camera = new THREE.PerspectiveCamera(120, window.innerWidth/window.innerHeight, 0.1, 500);
+  camera.position = (0, 0, 0);
+  // Camera facing upwards:
+  camera.rotation.x = 1.5;
+  camera.rotation.y = 0.27;
+  camera.rotation.z = 0.28;
+
+  light = new THREE.AmbientLight(0x404040);
+  scene.add(light);
+
+  renderer = new THREE.WebGLRenderer( { canvas: graphicCanvas} );
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  planeColor = backgroundColor;
+  geometry = new THREE.PlaneGeometry(planeSize, planeSize, planeSize);
+
+for ( let i=0; i<numIllustrations; i++ ) {
+    materials[i] = new THREE.MeshBasicMaterial( {
+    color: planeColor,
+    map: new THREE.TextureLoader().load('illustrations/edits/'+i+".JPG"),
+    side: THREE.DoubleSide,
+    wireframe: true
+})
+};
+  for ( let i = 0; i < numPlanes; i ++ ) {
+    planes[i] = new THREE.Mesh( geometry, materials[i%materials.length] );
+    planes[i].position.x = Math.random() * 1200 - 600;
+    planes[i].position.y = Math.random() * 600 - 300;
+    planes[i].position.z = Math.random() * 800 - 400;
+
+    planes[i].rotation.x = Math.random();
+    planes[i].rotation.y = Math.random();
+    planes[i].rotation.z = Math.random();
+    scene.add(planes[i]);
+  };
+
+  animate();
+  window.addEventListener('resize', onWindowResize, false);
+}
+function animate() {
+
+for(var i=0; i<numPlanes; i++) {
+  planes[i].rotation.x += Math.random()*0.01;
+  planes[i].rotation.y -= Math.random()*0.008;
+  planes[i].rotation.z += Math.random()*0.009;
+
+  planes[i].position.y -= Math.random()*0.6;
+  if (planes[i].position.y < -200) {
+    planes[i].position.y = 200;
+    planes[i].position = 0;
+  }
+};
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+function onWindowResize() {
+  camera.aspect = window.innerWidth/window.innerHeight;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.updateProjectionMatrix();
+}
+init();
