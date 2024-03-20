87457ae202032ab1c908d16b7e01085aaeefe483
Wilma Hultén <wilmahulten@gmail.com>
2024-03-20 18:21:35 +0100

    Play audio from random timecode, loop

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -5,11 +5,13 @@ const AudioPlayer = ({ audioSrc }) => {
   const audioRef = useRef(new Audio());
   const [isPlaying, setPlaying] = useState(false);
   const [audioContext, setAudioContext] = useState(null);
+  const [initialPlay, setInitialPlay] = useState(true);
 
   useEffect(() => {
     // Create a new audio element for each instance of AudioPlayer
     audioRef.current = new Audio();
     audioRef.current.src = audioSrc;
+    audioRef.current.loop = true;
 
     // If AudioContext doesn't exist, create it
     if (!audioContext) {
@@ -17,25 +19,41 @@ const AudioPlayer = ({ audioSrc }) => {
       setAudioContext(context);
     }
 
+    // Load metadata and set random start when available
+    audioRef.current.addEventListener('loadedmetadata', handleMetadataLoaded);
+
+    // Initialize volume to 0
+    audioRef.current.volume = 0;
+
     return () => {
-      // Clean up the AudioContext
+      // Clean up the AudioContext and event listener
       if (audioContext) {
         audioContext.close();
         setAudioContext(null);
       }
+      audioRef.current.removeEventListener('loadedmetadata', handleMetadataLoaded);
     };
   }, [audioSrc, audioContext]);
 
+  const handleMetadataLoaded = () => {
+    if (initialPlay && audioRef.current.duration !== Infinity) {
+      const randomStart = Math.random() * audioRef.current.duration;
+      audioRef.current.currentTime = randomStart;
+      setInitialPlay(false);
+    }
+  };
+
   const togglePlay = () => {
     const audioElement = audioRef.current;
+    const fadeDur = 2000;
 
     if (isPlaying) {
-      fadeOut(audioElement, 1000); // 1000 milliseconds (1 second) fade out duration
+      fadeOut(audioElement, fadeDur); // 1000 milliseconds (1 second) fade out duration
     } else {
       // If AudioContext exists, resume playback
       if (audioContext) {
         audioContext.resume().then(() => {
-          fadeIn(audioElement, 1000); // 1000 milliseconds (1 second) fade in duration
+          fadeIn(audioElement, fadeDur); // 1000 milliseconds (1 second) fade in duration
           audioElement.play();
           setPlaying(true);
         });
@@ -53,6 +71,7 @@ const AudioPlayer = ({ audioSrc }) => {
     const interval = duration / (finalVolume / increment);
 
     let volume = audioElement.volume;
+
     const fadeInterval = setInterval(() => {
       if (volume < finalVolume) {
         volume = Math.min(finalVolume, volume + increment); // Clamp volume between 0 and 1
